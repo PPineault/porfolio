6c794aee16e2e71085ea464cb897342b4381ee64
Patrick Pineault <patrick-pineault@hotmail.com>
2024-06-09 15:26:45 -0400

    magie-title

--- js/app.js
+++ js/app.js
@@ -75,3 +75,51 @@ backToTopBtn.addEventListener("click", function () {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+//animation h3,h3,h4
+// Fonction pour détecter si un élément est dans le champ de vision
+// Fonction pour détecter si un élément est dans le champ de vision
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Fonction pour ajouter/supprimer la classe "in-view" en fonction de la position de l'élément
+function animateOnScroll() {
+  const h2Elements = document.querySelectorAll("h2");
+  const h3Elements = document.querySelectorAll("h3");
+  const h4Elements = document.querySelectorAll("h4");
+
+  h2Elements.forEach((h2) => {
+    if (isInViewport(h2)) {
+      h2.classList.add("in-view");
+    } else {
+      h2.classList.remove("in-view");
+    }
+  });
+
+  h3Elements.forEach((h3) => {
+    if (isInViewport(h3)) {
+      h3.classList.add("in-view");
+    } else {
+      h3.classList.remove("in-view");
+    }
+  });
+
+  h4Elements.forEach((h4) => {
+    if (isInViewport(h4)) {
+      h4.classList.add("in-view");
+    } else {
+      h4.classList.remove("in-view");
+    }
+  });
+}
+
+// Attacher l'événement de défilement à la fonction d'animation
+window.addEventListener("scroll", animateOnScroll);
