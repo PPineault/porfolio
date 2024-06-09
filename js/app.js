cf77f799809793029a4c5950ba08f252bd22d118
Patrick Pineault <patrick-pineault@hotmail.com>
2024-06-09 09:56:01 -0400

    menu de navigation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+//hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
 //mediaelement
 const videoElements = document.querySelectorAll("video");
 
